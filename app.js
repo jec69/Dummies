2e5c082745143ed5bdc1c177b1abadae83d4f9d7
Angel Fernando Arvizu Meza <a23311109@uthermosillo.edu.mx>
2026-06-05 19:56:34 -0700

    agregar rutas CRUD de usuarios

--- app.js
+++ app.js
@@ -1,9 +1,49 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('CRUD de Usuarios');
+let usuarios = [
+    { id: 1, nombre: 'Juan' }
+];
+
+// Leer usuarios
+app.get('/usuarios', (req, res) => {
+    res.json(usuarios);
+});
+
+// Crear usuario
+app.post('/usuarios', (req, res) => {
+    const nuevoUsuario = {
+        id: usuarios.length + 1,
+        nombre: req.body.nombre
+    };
+
+    usuarios.push(nuevoUsuario);
+    res.json(nuevoUsuario);
+});
+
+// Actualizar usuario
+app.put('/usuarios/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const usuario = usuarios.find(u => u.id === id);
+
+    if (!usuario) {
+        return res.status(404).send('Usuario no encontrado');
+    }
+
+    usuario.nombre = req.body.nombre;
+    res.json(usuario);
+});
+
+// Eliminar usuario
+app.delete('/usuarios/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    usuarios = usuarios.filter(u => u.id !== id);
+
+    res.send('Usuario eliminado');
 });
 
 app.listen(3000, () => {
